d611bb5ca7aa278723b3aa784ffd59b091f4172b
Dwayne <saulsdwayne@gmail.com>
2016-10-03 10:01:06 +0200

    Just one more to finish

--- weekOrWeekend.js
+++ weekOrWeekend.js
@@ -1,11 +1,17 @@
 const assert = require('assert');
 var weekOrWeekend = function (day) {
-  if (day.startsWith('Wednesday')||(day.startsWith('Sunday')) ) {
-    return 'weekend';
+  if (day.startsWith('S')&&(day.endsWith('day')) ) {
+    return 'This is a weekend';
+  }
+  else if (day.endsWith('day')) {
+    return 'This is a weekday'
   }
     else {
-      return 'week';
+      return 'This is not a day';
     }
 }
-assert.equal(weekOrWeekend('Wednesday'), 'week');
-assert.equal(weekOrWeekend('Sunday'), 'weekend');
+assert.equal(weekOrWeekend('Wednesday'), 'This is a weekday');
+assert.equal(weekOrWeekend('Sunday'), 'This is a weekend');
+
+console.log('Wednesday'), 'This is a weekday');
+console.log('Sunday'), 'This is a weekend');
